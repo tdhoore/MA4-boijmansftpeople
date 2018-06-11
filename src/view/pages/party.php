f6d5451184192ba1e5d7430270e868e950026795
Billie Vanderhaeghen <billie.vanderhaeghen@student.howest.be>
2018-06-11 20:38:03 +0200

    mobile css party page

--- src/view/pages/party.php
+++ src/view/pages/party.php
@@ -1,76 +1,97 @@
-<section>
-  <article>
-    <h1>Arty Parties</h1>
-    <p>
-      Every last <strong>friday</strong> of the month <br/>
-      <strong>afterwork</strong> <br/>
-      drinks, music, <strong>ART</strong>
-    </p>
-  </article>
-  <article class="">
-    <h2>Next Arty Party:</h2>
-    <p>29/06</p>
-    <p>Biergarten</p>
-    <address class="">
-      Schiestraat 18 <br/>
-      3013BR Rotterdam <br/>
-      Netherlands
-    </address>
-  </article>
-<nav>
-  <ul>
-    <li>Open in Maps</li>
-    <li>I'm interested!</li>
-    <li>Tweet it!</li>
-  </ul>
-</nav>
+<section class="party-section1">
+    <header>
+      <h1 class="header-h1" data-content="arty parties" >Arty Parties</h1>
+    </header>
+    <section class="next-party-section">
+      <header>
+        <h2 class="next-party-title">Next Arty Party:</h2>
+      </header>
+      <section class="next-party-info">
+        <div class="next-party-date">
+          <p class="next-party-day">29</p>
+          <p class="next-party-month">06</p>
+        </div>
+        <p class="location-name" data-content="biergarten">Biergarten</p>
+        <address class="location-address">
+          Schiestraat 18 <br/>
+          3013BR Rotterdam <br/>
+          Netherlands
+        </address>
+      </section>
+    </section>
+    <div class="party-tagline">
+      <p>
+        Every last <strong>friday</strong> of the month <br/>
+        <strong>afterwork</strong> <br/>
+        drinks, music, <strong>ART</strong>
+      </p>
+    </div>
+    <nav class="social-link-nav">
+      <ul class="social-link-ul">
+        <li class="social-link-maps"><a class="social-a" href="#"><div class="social-icon"></div><span class="social-link-cta">Open in Maps</span></a></li>
+        <li class="social-link-fb"><a class="social-a" href="#"><div class="social-icon"></div><span class="social-link-cta">I'm interested!</span></a></li>
+        <li class="social-link-tt"><a class="social-a" href="#"><div class="social-icon"></div><span class="social-link-cta">Tweet it!</span></a></li>
+      </ul>
+    </nav>
 </section>
 
-<section>
-  <h2>upcoming Arty Parties:</h2>
-  <article class="">
-    <p>27/07</p>
-    <p>
+<section class="party-section2">
+  <header>
+    <h2 class="party-section2-title">Upcoming Arty Parties:</h2>
+  </header>
+  <article class="upcoming-party-article">
+    <div class="next-party-date">
+      <p class="next-party-day">27</p>
+      <p class="next-party-month">07</p>
+    </div>
+    <p class="upcoming-location-name">
       working site
       museum Boijmans
       van beuningen
     </p>
-    <address class="">
+    <address class="upcoming-address">
       Museumpark 18 </br>
-      3015 CX Rotterdam </br>
-      Netherlands
+      3015 CX Rotterdam
     </address>
   </article>
-  <article class="">
-    <p>31/08</p>
-    <p>Weena</p>
-    <address class="">
+  <article class="upcoming-party-article">
+    <div class="next-party-date">
+      <p class="next-party-day">31</p>
+      <p class="next-party-month">08</p>
+    </div>
+    <p class="upcoming-location-name">Weena</p>
+    <address class="upcoming-address">
       Weena 202 <br/>
-      3012 NJ Rotterdam <br/>
-      Netherlands
+      3012 NJ Rotterdam
     </address>
   </article>
-  <article class="">
-    <p>28/09</p>
-    <p>Ayla</p>
-    <address class="">
+  <article class="upcoming-party-article">
+    <div class="next-party-date">
+      <p class="next-party-day">28</p>
+      <p class="next-party-month">09</p>
+    </div>
+    <p class="upcoming-location-name">Ayla</p>
+    <address class="upcoming-address">
       Kruisplein 153 <br/>
-      3014 DD Rotterdam <br/>
-      Netherlands
+      3014 DD Rotterdam
     </address>
   </article>
-  <article class="">
-    <p>26/10</p>
-    <p>Markthal</p>
-    <address class="">
+  <article class="upcoming-party-article">
+    <div class="next-party-date">
+      <p class="next-party-day">26</p>
+      <p class="next-party-month">10</p>
+    </div>
+    <p class="upcoming-location-name">Markthal</p>
+    <address class="upcoming-address">
       Ds. Jan Scharpstraat 298 <br/>
-      3011 GZ Rotterdam <br/>
-      Netherlands
+      3011 GZ Rotterdam
     </address>
   </article>
 </section>
 
-<section>
-  <h2>We missed you here:</h2>
+<section class="party-section3">
+  <header>
+    <h2 class="party-section3-title">We missed you here:</h2>
+  </header>
   <img src="#" alt="Arty Party Mei">
 </section>

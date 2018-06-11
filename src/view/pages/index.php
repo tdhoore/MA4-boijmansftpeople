97040a9ba0b4ba2563c566ca7e16aa93531bd167
AnnabelDPQ <depourcqa@gmail.com>
2018-06-11 20:35:37 +0200

    css index tablet

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,32 +1,31 @@
 <section class="index-section1">
+  <h1 class="hide">Boijmans for the people</h1>
   <article class="index-article1">
-    <h1 class="hide">Boijmans for the people</h1>
-    <img src="../assets/img/shirley-temple-small.jpg" alt="Artwork of the month" width="320" height="240">
-    <p class="test">
-      Your art on our walls / 4 years / <strong>your art on our walls</strong> / 4 years / your art on our walls
-      4 years / your art on our walls / 4 years / your art on our walls / 4 years
-      Your art on our walls / <strong>4 years</strong> / your art on our walls / 4 years / your art on our walls
-      4 years / your art on our walls / 4 years / your art on our walls / 4 years
-    </p>
+    <h2 class="index-tagline"><span class="hide">4 years - your art on our walls</span></h2>
+    <img class="shirley" src="assets/img/shirley-temple-small.jpg" alt="Artwork of the month" width="320" height="240">
   </article>
 
-  <article class="index-article">
-    <h2 class="h2-article2">- theme of the month</h2>
-    <h3 class="h3-article2">Satire of sexualisation</h3>
-    <div class="div-article2">
-      <p class="p-article2">
-        This month’s work is <strong>“Shirley Temple, The Youngest, Most Sacred Monster of the Cinema in Her Time”</strong>, which was made by <strong>Salvador Dali</strong>, the well known surrealist painter. Shirley Temple was a child actress in the mid-thirties. Dali took her head out of a newspaper and superimposed it to the body of a lioness, made to look like a sphinx. That’s why they sometimes call it “Barcelona Sphinx”. The idea behind this work is described as a satire on the sexualisation of child stars by Hollywood.
-      </p>
-      <p class="p-article2">
-        <strong>Now it’s your turn</strong> to make a satire on sexualisation. You can start by <strong>downloading this painting</strong> to use as a template, or you can look through different submissions and use <strong>someone else’s submission as a template</strong>. Your only restriction is the size of your artwork should be the size of “Barcelona Sphinx” itself.
-      </p>
+  <article class="index-article index-article2">
+    <div class="a2-container">
+      <h2 class="h2-article2">- theme of the month</h2>
+      <h3 class="h3-article2">Satire of sexualisation</h3>
+      <div class="div-article2">
+        <p class="p-article">
+          This month’s work is <strong>“Shirley Temple, The Youngest, Most Sacred Monster of the Cinema in Her Time”</strong>, which was made by <strong>Salvador Dali</strong>, the well known surrealist painter. Shirley Temple was a child actress in the mid-thirties. Dali took her head out of a newspaper and superimposed it to the body of a lioness, made to look like a sphinx. That’s why they sometimes call it “Barcelona Sphinx”. The idea behind this work is described as a satire on the sexualisation of child stars by Hollywood.
+        </p>
+        <p class="p-article">
+          <strong>Now it’s your turn</strong> to make a satire on sexualisation. You can start by <strong>downloading this painting</strong> to use as a template, or you can look through different submissions and use <strong>someone else’s submission as a template</strong>. Your only restriction is the size of your artwork should be the size of “Barcelona Sphinx” itself.
+        </p>
+      </div>
     </div>
     <a href="index.php?page=submit" class="btn-article2">Submit your work</a>
   </article>
 
   <article class="index-article index-article3">
-    <h2>last month's winner</h2>
-    <img src="../assets/img/last-winner-small.jpg" class="last-winner" alt="last month's winner" width="253" height="272">
+    <h2 class="winner"><span class="hide">last month's winner</span></h2>
+    <div class="winner-div">
+      <img src="assets/img/last-winner-small.jpg" class="last-winner" alt="last month's winner" width="218" height="235">
+    </div>
   </article>
 </section>
 
@@ -40,32 +39,48 @@
 <section class="index-section3">
   <h2 class="s3-h2"><span class="hide">Arty parties</span></h2>
 
-  <article class="">
+  <article class="s3-article">
     <h3 class="s3a1-h3">upcoming Arty Party:</h3>
-    <p>29/06</p>
-    <p class="s3a1-title">Biergarten</p>
-    <address class="s3a2-address">
-      Schiestraat 18 3013BR <br/>
-      Rotterdam Netherlands
-    </address>
-    <a href="#">
-      <img src="../assets/img/interested-button.svg" alt="facebook event" width="50" height="50">
-    </a>
+
+    <div class="s3-div">
+
+      <div class="event">
+        <div class="event-div">
+          <p class="p-dag">29</p>
+          <p class="p-maand">06</p>
+        </div>
+
+          <div class="div-place">
+            <p class="s3a1-title">Biergarten</p>
+            <address class="s3a2-address">
+              Schiestraat 18 3013BR <br/>
+              Rotterdam Netherlands
+            </address>
+          </div>
+      </div>
+
+        <a href="#" class="fb-btn">
+          <img src="assets/img/interested-button.svg" alt="facebook event" width="50" height="50">
+        </a>
+
+      </div>
   </article>
 
-  <article class="">
+  <article class="index-section3">
     <h3 class="hide">Concept</h3>
-    <p>
+    <p class="p-article">
       Every last friday of the month, we throw an <strong>afterwork party</strong> to close off the theme of the month. We’ll announce <strong>who’s work will be displayed</strong> in Museum Boijmans Van Beuningen when it re-opens, and <strong>announce next month’s theme and painting</strong>. You can sit back and relax, look at everybody’s submission from the last few weeks, or you can do a <strong>collaborative remake</strong> of the new painting.
     </p>
     <a href="index.php?page=party" class="s3-btn">More about this</a>
   </article>
 </section>
 
-<section>
+<section class="index-section3 index-section4">
   <h2 class="hide">About</h2>
-  <img src="" alt="logo BFTP">
-  <p>
-    Each month while Boijmans Van Beuningen is closed, we put one painting from our collection on display for you. There’s a <strong>theme linked to each month</strong>, and your job is to <strong>remake the painting</strong> in question with that theme in mind. You can also choose to work on <strong>someone else’s interpretation</strong> of the painting.
-  </p>
+  <img class="aboutimg" src="assets/img/logo-bftp-about.svg" alt="logo BFTP" width="165" height="165">
+  <div class="about-bg">
+    <p  class="p-article">
+      Each month while Boijmans Van Beuningen is closed, we put one painting from our collection on display for you. There’s a <strong>theme linked to each month</strong>, and your job is to <strong>remake the painting</strong> in question with that theme in mind. You can also choose to work on <strong>someone else’s interpretation</strong> of the painting.
+    </p>
+  </div>
 </section>

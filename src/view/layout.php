a31a46c3acba5874e9199a9395e2ef1acf85785a
tdhoore <timdhoore@gmail.com>
2018-06-13 08:58:01 +0200

    added favicon

--- src/view/layout.php
+++ src/view/layout.php
@@ -29,7 +29,24 @@
     <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     <title>BFTP</title>
     <?php echo $css;?>
-
+    <link rel="shortcut icon" href="assets/img/favico/favicon.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" sizes="57x57" href="assets/img/favico/apple-icon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="assets/img/favico/apple-icon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="assets/img/favico/apple-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="assets/img/favico/apple-icon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="assets/img/favico/apple-icon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="assets/img/favico/apple-icon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="assets/img/favico/apple-icon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="assets/img/favico/apple-icon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="assets/img/favico/apple-icon-180x180.png">
+    <link rel="icon" type="image/png" sizes="192x192"  href="assets/img/favico/android-icon-192x192.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="assets/img/favico/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="assets/img/favico/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="assets/img/favico/favicon-16x16.png">
+    <link rel="manifest" href="assets/img/favico/manifest.json">
+    <meta name="msapplication-TileColor" content="#323392">
+    <meta name="msapplication-TileImage" content="assets/img/favico/ms-icon-144x144.png">
+    <meta name="theme-color" content="#323392">
 </head>
 
 <body>
@@ -44,7 +61,7 @@
             </a>
           </li>
           <li>
-            <a href="https://www.boijmans.nl/">
+            <a href="https://www.boijmans.nl/" target="_blank">
               <img class="bvb" src="assets/img/mbvb-logo.svg" alt="i.s.m. museum BVB" width="200" height="55">
             </a>
           </li>

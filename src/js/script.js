1317fa15cb06f4ab978cb5d2372475ca50661ff2
AnnabelDPQ <depourcqa@gmail.com>
2018-06-13 11:52:25 +0200

    css 1024 submit

--- src/js/script.js
+++ src/js/script.js
@@ -5,6 +5,10 @@ import InfiniteScroll from './classes/InfiniteScroll';
 import Validator from './classes/Validator';
 import Emitter from 'event-emitter';
 
+let $pCheck;
+let $inputFile;
+let interval;
+
 const eventEmitter = Emitter();
 
 const customSelect = new CustomSelect({selector: `select`, customClass: `customDropDown`, customOpenClass: `customDropDownOpen`, customSelectedClass: `customSelectedItem`, emmitter: eventEmitter});
@@ -17,7 +21,47 @@ const infiniteScroll = new InfiniteScroll({selector: `.infinite`, customClass: `
 
 const submitValidator = new Validator({formSelector: `.submitForm`});
 
+const onFileChange = e => {
+  interval = setInterval(changeInput, 1000);
+}
+
+const changeInput = () => {
+  if ($inputFile.value !== '') {
+    $pCheck.innerHTML = $inputFile.value.replace(/([^\\]*\\)*/, '');
+    clearInterval(interval);
+  }
+}
+
+const styleInputFile = () => {
+    let $div = document.querySelector('.drop-div');
+    $div.classList.add('drop-div');
+
+    $inputFile = document.querySelector('.submit-drop');
+    $inputFile.classList.add('input-file-styling');
+
+    let $divFlex = document.createElement('div');
+    $divFlex.classList.add('align-input-file');
+
+    let $p = document.createElement('p');
+    $p.classList.add('input-file-js-on');
+
+    let $strong = document.createElement('strong');
+    $strong.innerHTML = 'Select File';
+
+    $pCheck = document.createElement('p');
+    $pCheck.classList.add('file-check');
+    $pCheck.innerHTML = 'no file selected';
+
+    $div.appendChild($divFlex);
+    $divFlex.appendChild($p);
+    $divFlex.appendChild($pCheck);
+    $p.appendChild($strong);
+  }
+
 const init = () => {
+  styleInputFile();
+  document.querySelector('.submit-drop').addEventListener('click', onFileChange);
+
   filter.init();
 
   customSelect.init();
@@ -48,7 +92,7 @@ const init = () => {
         name: `valueMissing`,
         messages: {
           error: `Sorry, leaving it anonymous isn’t an option`,
-          okey: `What a beautiful name`
+          okey: `What a beautiful name!`
         }
       }
     ]);
--- src/view/pages/submit.php
+++ src/view/pages/submit.php
@@ -1,14 +1,26 @@
 <section class="submit-s1">
-  <h1 data-content="submit" class="submit-s1h1">Submit</h1>
-  <h2 class="submit-s1h2">your work</h2>
+  <h1 class="hide">Submit your work</h1>
+  <div class="s1-div">
+    <h2 data-content="submit" class="submit-s1h1">Submit</h2>
+    <h2 data-content="your work" class="submit-s1h2">your work</h2>
+  </div>
 
   <article class="s1a1">
       <h2 class="hide">Upload</h2>
-      <img src="assets/img/shirley-temple-small.jpg" alt="art of the month" width="320" height="240" class="s1-img">
-      <p class="s1-p">you're working on <strong class="s1-p-strong">Shirley Temple</strong> by Salvador Dali</p>
-      <a class="download-btn" href="#">Download</a>
-      <p class="or">or</p>
-      <a href="#" class="upload-btn">Upload your artwork</a>
+      <picture class="s1-img">
+        <source srcset="assets/img/shirley-temple-small.jpg" media="(max-width: 1023px)">
+        <source srcset="assets/img/shirley-temple-big.jpg" media="(min-width: 1024px)">
+        <img class="s1-img" src="assets/img/shirley-temple-small.jpg" alt="art of the month template"
+        sizes="(max-width: 1023px) 100vw,
+                (min-width: 1024px) 44vw">
+      </picture>
+
+      <div class="s1a1-div">
+        <p class="s1-p">you're working on <strong class="s1-p-strong">Shirley Temple</strong> by Salvador Dali</p>
+        <a class="download-btn" href="#">Download</a>
+        <p class="or">or</p>
+        <a href="#" class="upload-btn">Upload your artwork</a>
+      </div>
   </article>
 
   <article class="s1a2">
@@ -19,54 +31,69 @@
 
       <input type="hidden" name="action" value="submitArt">
 
-      <label for="artistname">
-        <span class="titleAccent upload-lbl">Artist name</span>
-        <div class="textInput">
-          <input type="text" name="artistname" id="artistname" placeholder="Annabellini" required class="submit-input"/>
-        </div>
-        <span class="validator"></span>
-      </label>
+      <div class="subform-div">
+          <div class="nameTitle">
+            <label for="artistname">
+              <span class="titleAccent upload-lbl">Artist name</span>
+              <div class="textInput">
+                <input type="text" name="artistname" id="artistname" placeholder="Annabellini" required class="submit-input"/>
+              </div>
+              <span class="validator"></span>
+            </label>
 
-      <label for="title">
-        <span class="titleAccent upload-lbl">Name your artwork</span>
-        <div class="textInput">
-          <input type="text" name="title" id="title" placeholder="Shirley Temple" required class="submit-input"/>
-        </div>
-        <span class="validator"></span>
-      </label>
+            <label for="title">
+              <span class="titleAccent upload-lbl">Name your artwork</span>
+              <div class="textInput">
+                <input type="text" name="title" id="title" placeholder="Shirley Temple" required class="submit-input"/>
+              </div>
+              <span class="validator"></span>
+            </label>
+          </div>
 
-      <label for="email">
-        <span class="titleAccent upload-lbl">Email address</span>
-        <div class="textInput">
-          <input type="email" name="email" id="email" placeholder="salvadordali04@gmail.com" required class="submit-input"/>
-        </div>
-        <span class="validator"></span>
-      </label>
+                <label for="email">
+          <span class="titleAccent upload-lbl">Email address</span>
+          <div class="textInput">
+            <input type="email" name="email" id="email" placeholder="salvadordali04@gmail.com" required class="submit-input"/>
+          </div>
+          <span class="validator"></span>
+                </label>
 
-      <div class="dragAndDrop">
-        <label for="artwork" class="upload-lbl">Drop your art here</label>
-        <input type="file"
-         name="artwork"
-         accept="image/png, image/jpeg, image/gif" class="submit-input"/>
+                 <label for="tags">
+           <span class="titleAccent upload-lbl">tags</span>
+           <div class="textInput">
+             <textarea name="tags" id="tags" class="textarea" placeholder="seperate tags with comma’s"></textarea>
+           </div>
+           <span class="validator"></span>
+                 </label>
+        </div>
+
+       <div class="fuckingshit">
+         <div class="dragAndDrop">
+           <label for="artwork" class="drop-lbl">Drop your art here</label>
+
+             <div class="drop-div">
+               <input type="file"
+                name="artwork"
+                accept="image/png, image/jpeg, image/gif" class="submit-drop"/>
+             </div>
+
+          </div>
+
+          <label for="terms">
+            <div class="terms-div">
+              <span class="titleAccent agree">I agree with the terms and conditions</span>
+              <div class="textInput">
+                <input class="checkbox" type="checkbox" name="terms" id="terms" required>
+              </div>
+            </div>
+            <span class="validator"></span>
+          </label>
+
+               <div class="submitSubmit">
+          <input type="submit" name="submit" value="Submit your work" class="submitSubmit">
+               </div>
        </div>
 
-       <label for="tags">
-         <span class="titleAccent upload-lbl">tags</span>
-         <div class="textInput">
-           <textarea name="tags" id="tags"></textarea>
-         </div>
-         <span class="validator"></span>
-       </label>
-
-       <label for="terms">
-         <span class="titleAccent">I agree with the terms and conditions</span>
-         <div class="textInput">
-           <input type="checkbox" name="terms" id="terms" required>
-         </div>
-         <span class="validator"></span>
-       </label>
-
-      <input type="submit" name="submit" value="Submit your work">
     </form>
   </article>
 </section>
